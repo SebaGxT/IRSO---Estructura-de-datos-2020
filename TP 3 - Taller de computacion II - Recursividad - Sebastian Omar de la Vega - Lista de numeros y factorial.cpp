92b0d350ccb40a65d8714d718fa6a6bd30786169
Sebastian Omar de la Vega <61508227+SebaGxT@users.noreply.github.com>
2020-09-18 20:09:26 -0300

    TP° 3 - Taller de computación II

---
+++ TP 3 - Taller de computacion II - Recursividad - Sebastian Omar de la Vega - Lista de numeros y factorial.cpp
@@ -0,0 +1,98 @@
+//Librerias
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//Funciones y procedimientos
+
+void pausar(){
+	
+	printf("\n\n\n\n");
+	system("pause");
+	
+}
+
+void leer_positivo(int &n){	
+		
+	printf("\nIngrese un valor positivo: ");
+	scanf("%d",&n);
+	fflush(stdin);
+	
+	while(n<0){
+		
+		printf("\n\nPor favor ingrese un valor positivo.\n");
+		scanf("%d",&n);
+		fflush(stdin);
+		
+	}	
+					
+}
+
+bool par_impar(int n){
+	
+	if(n%2==0){
+		
+		return true;
+		
+	}else{
+		
+		return false;
+		
+	}
+	
+}
+
+int factorial(int n){
+	
+	int res;
+	
+	if(n==0){
+		
+		res=1;
+		
+	}else{
+		
+		res=n*factorial(n-1);
+		
+	}
+	
+	return res;
+	
+}
+
+void factoriales(int n){
+	
+	
+	if(n>0){
+		
+		printf("%d ..... factorial: %d\n",n,factorial(n));
+		factoriales(n-1);
+		
+	}
+	
+}
+
+//Main
+int main(){
+	
+//Variables
+	
+	int n1;
+	
+//Ingreso de datos
+	
+	leer_positivo(n1);
+	
+
+//Salida de datos
+	
+	printf("\n");
+	factoriales(n1);
+	
+
+//Pausa y fin de programa
+	
+	pausar();
+	return 0;
+	
+}
