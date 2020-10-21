9e587f82c0938fe1884298253093c6ff1b35fc61
Sebastian Omar de la Vega <61508227+SebaGxT@users.noreply.github.com>
2020-10-21 11:29:50 -0300

    Corrección del Parcial 1 - Estructura de datos

--- Parcial 1 - Estructura de datos - Sebastian Omar de la Vega.cpp
+++ Parcial 1 - Estructura de datos - Sebastian Omar de la Vega.cpp
@@ -56,14 +56,14 @@ void imp_lisse(struct lista *ini){
 	}
 	
 }
-
+//muy bien
 /*1- Codificar un procedimiento que lea nros. hasta que aparezca un 0 y cargar una lista simplemente encadenada  con dichos nros. donde los mismos queden insertados de 
 atrás hacia adelante.  Es decir, si se leen 1, 2, 3, 4, 0 se generara la LSE 4,  3, 2 , 1.
 Su declaración será   void cargareves (struct lista *&com)*/
 void cargareves(struct lista *&ini){
 	
 	struct lista *punt,*nue;
-	int n,aux;
+	int n;
 	
 	leer_nro_e("\nIngrese un valor: ",n);
 	
@@ -82,16 +82,12 @@ void cargareves(struct lista *&ini){
 		
 		while(n!=0){
 			
-			if(n!=0){
-				
-				nue=new struct lista;
-				nue->num=n;
-				nue->sig=NULL;
-				nue->sig=punt;
-				ini=nue;
-				punt=ini;
-				
-			}
+			nue=new struct lista;
+			nue->num=n;
+			nue->sig=NULL;
+			nue->sig=punt;
+			ini=nue;
+			punt=ini;
 			
 			leer_nro_e("\nIngrese un valor: ",n);
 			
@@ -100,7 +96,7 @@ void cargareves(struct lista *&ini){
 	}
 	
 }
-
+//muy bien
 /*2- Codificar un procedimiento que muestre los contenidos impares de los nodos de una lista simplemente encadenada en forma recursiva.  
 (Aclaración: es el contenido par, y no la posición par).
 Su declaración será   void mostrarrecursivaimpar (struct lista *com)*/
@@ -115,18 +111,16 @@ void mostrarrecursivaimpar(struct lista *ini){
 		if(!par_impar(punt->num)){
 			
 			printf("\n%d",punt->num);
-			mostrarrecursivaimpar(punt->sig);
-			
-		}else{
-			
-			mostrarrecursivaimpar(punt->sig);
 			
 		}
 			
+		mostrarrecursivaimpar(punt->sig);
+					
 	}
 	
 }
-
+//muy bien podes poner la recursividad una sola vez pues se repite como 
+//final de ambas partes del if
 /*3- Codificar una función que  reciba una lista simplemente encadenada y devuelva verdadero si tiene por lo menos dos nodos y falso en caso contrario. 
 Su declaración será   bool tienedos (struct lista  *com)*/
 bool tienedos(struct lista *ini){
@@ -154,7 +148,7 @@ bool tienedos(struct lista *ini){
 	return valid;	
 	
 }
-
+//muy bien
 /*4- Codificar un algoritmo que reciba una lista simplemente  encadenada y le  elimine  el segundo elemento si la lista tiene por lo menos dos nodos. 
 Para ello, se invocará a la funcion tienedos del ejercicio 3.
 Su declaración será   void eliminarsegundo (struct lista *&com).*/
@@ -170,7 +164,7 @@ void eliminarsegundo(struct lista *ini){
 	
 	if(tienedos(punt)){
 		
-		while(punt!=NULL&&cont!=2){
+		while(punt!=NULL&&cont<2){
 			
 			cont++;
 			ant=punt;
@@ -180,7 +174,9 @@ void eliminarsegundo(struct lista *ini){
 		
 		if(punt->sig==NULL){
 			
+			ant->sig=punt->sig;
 			delete(punt);
+			punt=ant->sig;
 			
 		}else{
 			
@@ -198,7 +194,7 @@ void eliminarsegundo(struct lista *ini){
 	
 	
 }
-
+//muy bien
 /*5- Codificar un programa principal (main) que cargue una lista simplemente encadenada desde el comienzo, muestre los impares de la misma por pantalla. 
 Luego elimine el segundo nodo si lo tuviera y la muestre. */
 
@@ -209,7 +205,7 @@ int main(){
 	
 	struct lista *in;
 	
-//Ingreso de datos
+//Ingreso de dataos
 	
 	cargareves(in);
 
@@ -238,4 +234,4 @@ int main(){
 	return 0;
 
 }
-
+//muy bien
