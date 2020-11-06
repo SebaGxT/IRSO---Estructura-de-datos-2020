94740c9fe4f782ddb4a6e004bfb5bacb022b6179
Sebastian Omar de la Vega <61508227+SebaGxT@users.noreply.github.com>
2020-11-06 19:22:30 -0300

    Parcial 1 - Taller de computación II

---
+++ Parcial 1 - Taller de computación II - Sebastian Omar de la Vega.cpp
@@ -0,0 +1,396 @@
+//Librerias
+#include <stdio.h>
+#include <stdlib.h>
+
+//Arbol
+struct arbol{
+	
+	int num;
+	struct arbol *der;
+	struct arbol *izq;
+		
+};
+
+//Funciones y Procedimientos
+
+void pausar(){
+
+	printf("\n\n\n\n");
+	system("pause");
+
+}
+
+void leer_nro_e(char cade[30],int &n){
+	
+	printf("\n %s",cade);
+	fflush(stdin);	
+	scanf("%d",&n);
+
+}
+
+void crear_arbol(struct arbol *&ra){
+	
+	ra=NULL;
+	
+}
+
+bool buscanro_arbol(struct arbol *&ra, int n){
+	
+	struct arbol *punt;
+	bool valid;
+	
+	valid=false;
+	punt=ra;
+	
+	while(punt!=NULL&&valid==false){
+		
+		if(punt->num==n){
+			
+			valid=true;
+			
+		}else{
+			
+			if(punt->num>n){
+				
+				punt=punt->izq;
+				
+			}else{
+				
+				punt=punt->der;
+				
+			}
+			
+		}
+		
+	}
+	
+	return valid;
+	
+}
+
+void insert_arbol(struct arbol *&ra,int n){
+	
+	struct arbol *punt,*ant,*nue;
+	
+	nue=new struct arbol;
+	nue->num=n;
+	nue->der=NULL;
+	nue->izq=NULL;
+	
+	if(ra!=NULL){
+		
+		punt=ra;
+		ant=ra;
+		
+		while(punt!=NULL){
+			
+			ant=punt;
+			
+			if(punt->num>n){
+				
+				punt=punt->izq;
+				
+			}else{
+				
+				punt=punt->der;
+				
+			}
+			
+		}
+		
+		if(ant->num>n){
+			
+			ant->izq=nue;
+			
+		}else{
+			
+			ant->der=nue;
+			
+		}
+		
+	}else{
+		
+		ra=nue;
+		
+	}
+	
+}
+
+void imp_arbol_rec(struct arbol *ra){
+	
+	if(ra!=NULL){
+		
+		//IRD
+		imp_arbol_rec(ra->izq);
+		printf("\n%d",ra->num);
+		imp_arbol_rec(ra->der);
+		
+	}
+	
+}
+
+void borrar_hoja(struct arbol *&ra, struct arbol *punt, struct arbol *ant, int n){
+	
+	if(punt==ra){
+		
+		ra=NULL;
+		
+	}else{
+		
+		if(ant->izq==punt){
+			
+			ant->izq=NULL;
+			
+		}else{
+			
+			ant->der=NULL;
+			
+		}
+		
+	}
+	
+	delete(punt);
+	
+}
+
+void borrar_un_hijo(struct arbol *&ra, struct arbol *punt, struct arbol *ant, int n){
+	
+	if(punt==ra){
+		
+		if(ra->izq!=NULL){
+			
+			ra=ra->izq;
+			
+		}else{
+			
+			ra=ra->der;
+			
+		}
+		
+	}else{
+		
+		if(ant->izq==punt){
+			
+			if(punt->izq!=NULL){
+				
+				ant->izq=punt->izq;
+				
+			}else{
+				
+				ant->izq=punt->der;
+				
+			}
+			
+		}else{
+			
+			if(punt->izq!=NULL){
+				
+				ant->der=punt->izq;
+				
+			}else{
+				
+				ant->der=punt->der;
+				
+			}		
+			
+		}
+		
+	}
+	
+	delete(punt);
+	
+}
+
+void borrar_dos_hijos(struct arbol *&ra, struct arbol *punt, struct arbol *ant, int n){
+	
+	struct arbol *izqu,*dere,*an;
+	
+	izqu=punt->izq;
+	an=izqu;
+	dere=an;
+	
+	while(dere!=NULL){
+		
+		an=dere;
+		dere=dere->der;
+		
+	}
+	
+	if(izqu->der!=NULL){
+		
+		an->der=dere->izq;
+		punt->num=dere->num;
+		delete(dere);
+		
+	}else{
+		
+		punt->izq=izqu->izq;
+		punt->num=izqu->num;
+		delete(izqu);
+		
+	}
+	
+}
+
+void borrar_nodo_arbol(struct arbol *&ra, struct arbol *punt, struct arbol *ant, int n){
+	
+	if(punt->izq==NULL&&punt->der==NULL){
+		
+		borrar_hoja(ra,punt,ant,n);
+		
+	}else{
+		
+		if(punt->izq!=NULL&&punt->der!=NULL){
+			
+			borrar_dos_hijos(ra,punt,ant,n);
+			
+		}else{
+			
+			borrar_un_hijo(ra,punt,ant,n);
+			
+		}
+		
+	}
+	
+}
+
+void eliminar_arbol(struct arbol *&ra, int n){
+	
+	struct arbol *punt,*ant;
+	bool valid;
+	
+	valid=false;
+	
+	punt=ra;
+	ant=punt;
+	
+	while(valid==false){
+		
+		if(punt->num==n){
+			
+			valid=true;
+			
+		}else{
+			
+			ant=punt;
+			
+			if(punt->num>n){
+			
+				punt=punt->izq;
+			
+			}else{
+			
+				punt=punt->der;
+			
+			}
+		
+		}	
+		
+	}
+	
+/*	while(punt->num!=n){
+	
+		ant=punt;
+			
+		if(punt->num>n){
+			
+			punt=punt->izq;
+			
+		}else{
+			
+			punt=punt->der;
+			
+		}
+				
+	}
+	*/
+	
+	borrar_nodo_arbol(ra,punt,ant,n);
+		
+}
+
+//Main
+int main(){
+
+//Variables
+	
+	struct arbol *ar;
+	int n,contrep,conteli;
+	
+	contrep=0;
+	conteli=0;
+	crear_arbol(ar);
+	
+//Ingreso de datos
+	
+	printf("Carga de un arbol, finaliza con 0.\n\n");
+	
+	leer_nro_e("Ingrese un valor: ",n);
+	
+	if(n!=0){
+		
+		while(n!=0){
+			
+			if(!buscanro_arbol(ar,n)){
+			
+				insert_arbol(ar,n);
+			
+			}else{
+			
+				printf("\n\nEl n%cmero ingresado esta en el Arbol\n\n",163);
+				contrep++;
+				
+			}
+			
+			leer_nro_e("\nIngrese un valor: ",n);
+				
+		}
+		
+//Proceso
+	
+	printf("\n\nEliminaci%cn de valores del arbol, finaliza con 0.\n\n",162);
+	leer_nro_e("\nIngrese un valor a eliminar: ",n);
+	if(n!=0){
+			
+		while(n!=0){
+				
+			if(buscanro_arbol(ar,n)){
+					
+				eliminar_arbol(ar,n);
+				conteli++;
+					
+			}else{
+					
+				printf("\n\nEl n%cmero ingresado no esta en el Arbol\n\n",163);
+					
+			}
+				
+			leer_nro_e("\nIngrese un valor a eliminar: ",n);
+				
+		}
+			
+	}else{
+			
+		printf("\n\nNo se eliminaron valores del Arbol.\n\n");
+			
+	}
+
+//Salida de datos		
+	
+	printf("\n\nArbol IRD de menor a mayor\n\n");
+	imp_arbol_rec(ar);
+	printf("\n\nCantidad de n%cmeros repetidos en el ingreso: %d",163,contrep);
+	printf("\n\nCantidad de n%cmeros eliminados: %d",163,conteli);	
+		
+	}else{
+		
+		printf("\n\nNo se ingresaron valores, finaliza el programa.\n\n");
+		
+	}
+	
+//Pausa y fin de programa
+
+	pausar();
+	return 0;
+
+}
